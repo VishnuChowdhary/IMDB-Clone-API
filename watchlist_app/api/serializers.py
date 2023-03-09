3d6402c7a524e0b70abd37d2d1d4cbdfb6f97624
VishnuChowdhary <112419086+VishnuChowdhary@users.noreply.github.com>
2023-03-10 00:54:59 +0530

    defining validators to fields and objects

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -16,3 +16,18 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('name', instance.active)
         instance.save()
         return instance
+    
+    # Object Level Validator
+    def validate(self, data):
+        if data['title'] == data['description']:
+            return serializers.ValidationError("Title and Description should be diffrent")
+        else:
+            return data
+    
+    # Field Level validator
+    def validate_name(self, value):
+        if len(value) < 2:
+            return serializers.ValidationError("Name is Too short")
+        else:
+            return value
+        
